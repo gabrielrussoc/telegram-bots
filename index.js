6943649063bd0d5e5791415351ac8c8fa61e085d
Victor Sena Molero <victorsenam@gmail.com>
2015-12-09 01:25:19 -0200

    pipe server running

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var russobot = require('./russobot.js');
 var joaobot = require('./joaobot.js');
 var lispectorbot = require('./lispector.js');
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  request.pipe(response);
+}).listen(process.env.PORT || 5000);
