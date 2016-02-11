c4e2fa0f8ac37e905e88e73c40ab2b20e76ca6ed
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-02-11 01:35:51 -0200

    agora vai

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var russobot = require('./russobot.js');
 var joaobot = require('./joaobot.js');
 var lispectorbot = require('./lispector.js');
 
-//var http = require('http');
-//
-//var server = http.createServer(function(request, response) {
-//  request.pipe(response);
-//}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var http = require('http');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-server.listen(server_port, server_ip_address, function () {
-console.log( "Listening on " + server_ip_address + ", server_port " + port )
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+          response.write("Welcome to Node.js on OpenShift!\n\n");
+          response.end("Thanks for visiting us! \n");
 });
+
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
