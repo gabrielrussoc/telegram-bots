5e5b18c02984520fea5ed42164775ab82da1d461
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-02-11 01:20:02 -0200

    segunda tentativa pra mudar o server

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var russobot = require('./russobot.js');
 var joaobot = require('./joaobot.js');
 var lispectorbot = require('./lispector.js');
 
-var http = require('http');
+//var http = require('http');
+//
+//var server = http.createServer(function(request, response) {
+//  request.pipe(response);
+//}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
-var server = http.createServer(function(request, response) {
-  request.pipe(response);
-}).listen(process.env.OPENSHIFT_NODEJS_PORT || 5000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
