b83552ad856501c70098a7b68b827181be25e066
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-04-06 21:13:14 -0300

    lado bom da vida

--- russobot.js
+++ russobot.js
@@ -3,6 +3,22 @@ var token = '133436018:AAHHVXvAkq7oIaLY2pNj4Pwhsu4pnneEG50';
 
 var bot = new Bot(token,{polling: true});
 
+bot.onText(/lado bom da vida/i,function(msg) {
+  var chatId = msg.chat.id;
+  var n = Math.random() * 9;
+  var ret;
+  if(n < 1) ret = 'pao de alho';
+  else if (n < 2) ret = 'acordar cedo e lembrar que é sábado';
+  else if (n < 3) ret = 'mijar apertado';
+  else if (n < 4) ret = 'passar naf rente de uma loja com ar condicionado num dia de muito calor';
+  else if (n < 5) ret = 'quando chega o que vc comprou pela internet';
+  else if (n < 6) ret = 'quando a pizza chega rápido';
+  else if (n < 7) ret = 'achar dinheiro no bolso';
+  else if (n < 8) ret = 'wifi grátis';
+  else ret = 'lombo no central';
+
+});
+
 bot.onText(/.+ nao .+ oq aconteceu/,function(msg) {
   var chatId = msg.chat.id;
   bot.sendMessage(chatId,'os caras do Charlie Brown invadiram a cidade');
